cc8c08bd016fe0b4eb57ac50eaab2092c602ad01
Isaac Amaro <isaac.amaro@philips.com>
2022-12-09 18:36:04 -0300

    Added test bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,20 @@
 terraform {
-    backend "s3" {
-        bucket = "terraform-backend-0716742c2f2f9"
-        key    = "lambda-api/terraformstate"
-        region = "us-east-1"
-    }
+  backend "s3" {
+    bucket = "terraform-backend-0716742c2f2f9"
+    key    = "lambda-api/terraformstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "test-bucket-a8f5f167f44f4964e"
+
+  tags = {
+    Name = "My bucket"
+  }
+}
+
+resource "aws_s3_bucket_acl" "test-bucket-acl" {
+  bucket = aws_s3_bucket.test-bucket.id
+  acl    = "private"
 }
